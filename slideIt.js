482feed7df61ab4150ead1b65554f81b07a53e6c
deller26 <deller26@gmail.com>
2015-07-17 09:48:57 -0700

    Update slideIt.js

--- slideIt.js
+++ slideIt.js
@@ -1,28 +1,28 @@
 $(function() {
 
 	var slides    = $('#home-slider li'),
-			textIndex = 0,
-		  slideText = [
-		  							{
-											headLine : 'public records search service',
-											subHead  : 'Background Checks, Police Records, Court Documents, Contact Information and Much More!'
-										},
-										{
-											headLine : 'background checks',
-											subHead  : 'Is there a felon, sex offender or former inmate on your block? Search criminal records 
-																	using Instant Checkmate and find out.'
-										},
-										{
-											headLine : 'the information you want',
-											subHead  : 'Instant Checkmate takes the hassle out of uncovering someone’s past by making it easy to 
-																	run a background check online and on the go.'
-										},
-										{
-											headLine : 'expose the truth',
-											subHead  : 'With Instant Checkmate you can discover truths, expose lies, and get the real story about 
-																	the people in your life.'
-										}
-									];
+		textIndex = 0,
+		slideText = [
+  			{
+					headLine : 'public records search service',
+					subHead  : 'Background Checks, Police Records, Court Documents, Contact Information and Much More!'
+				},
+				{
+					headLine : 'background checks',
+					subHead  : 'Is there a felon, sex offender or former inmate on your block? Search criminal records 
+											using Instant Checkmate and find out.'
+				},
+				{
+					headLine : 'the information you want',
+					subHead  : 'Instant Checkmate takes the hassle out of uncovering someone’s past by making it easy to 
+											run a background check online and on the go.'
+				},
+				{
+					headLine : 'expose the truth',
+					subHead  : 'With Instant Checkmate you can discover truths, expose lies, and get the real story about 
+											the people in your life.'
+				}
+			];
 
 	// fadeIn slider 
 	$('#home-slider').fadeIn(1000);
